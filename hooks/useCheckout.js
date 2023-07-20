333f1e37a6f6854b90d3eeb8332cafb6a9d67e31
Ethan Ye <chiguayeshao@gmail.com>
2023-07-20 19:56:00 +0800

    Update useCheckout.js

--- hooks/useCheckout.js
+++ hooks/useCheckout.js
@@ -30,7 +30,6 @@ export const usePersonalCheckout = () => {
       setIsPersonalCheckoutLoading(false)
     }
   }
-
   return {
     personalCheckout: handleCheckout,
     isPersonalCheckoutLoading: isPersonalCheckoutLoading

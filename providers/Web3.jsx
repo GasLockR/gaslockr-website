563aa44190b4d77ceb95a7b89c39a563c03c5abd
Ethan Ye <chiguayeshao@gmail.com>
2023-08-13 16:35:11 +0800

    Update Web3.jsx

--- providers/Web3.jsx
+++ providers/Web3.jsx
@@ -20,7 +20,7 @@ import { publicProvider } from "wagmi/providers/public"
 const { chains, publicClient, webSocketPublicClient } = configureChains(
   [
     // mainnet,
-    sepolia
+    goerli
     // polygon,
     // optimism,
     // arbitrum,

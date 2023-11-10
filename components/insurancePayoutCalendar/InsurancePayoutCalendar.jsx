050ce11c6bd5a7161729f7c5b698ec2d27417a5b
Ethan Ye <chiguayeshao@gmail.com>
2023-11-11 01:58:20 +0800

    update calender

--- components/insurancePayoutCalendar/InsurancePayoutCalendar.jsx
+++ components/insurancePayoutCalendar/InsurancePayoutCalendar.jsx
@@ -18,7 +18,7 @@ function InsurancePayoutCalendar({ payoutDates }) {
           <div className="flex flex-row gap-2">
             <div>Total Payout:</div>
             {/* 0.003 ETH / 次 */}
-            <div className="text-[#57C5B6]">1.238</div>
+            <div className="text-[#57C5B6]">{62 * 0.003}</div>
             <div>ETH</div>
           </div>
         </div>

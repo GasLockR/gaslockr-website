f373fd2ddea6331e8aa3ce9eb6eaafe22bbcb909
Ethan Ye <chiguayeshao@gmail.com>
2023-11-10 12:22:24 +0800

    remove mock data

--- components/pricing/Pricing.jsx
+++ components/pricing/Pricing.jsx
@@ -23,59 +23,6 @@ import { Container } from "../Container"
 import InsurancePayoutCalendar from "../insurancePayoutCalendar/InsurancePayoutCalendar"
 import IncomeChart from "../incomeChart/IncomeChart"
 
-const data = [
-  ["2000-06-05", 116],
-  ["2000-06-06", 129],
-  ["2000-06-07", 135],
-  ["2000-06-08", 86],
-  ["2000-06-09", 73],
-  ["2000-06-10", 85],
-  ["2000-06-11", 73],
-  ["2000-06-12", 68],
-  ["2000-06-13", 92],
-  ["2000-06-14", 130],
-  ["2000-06-15", 245],
-  ["2000-06-16", 139],
-  ["2000-06-17", 115],
-  ["2000-06-18", 111],
-  ["2000-06-19", 309],
-  ["2000-06-20", 206],
-  ["2000-06-21", 137],
-  ["2000-06-22", 128],
-  ["2000-06-23", 85],
-  ["2000-06-24", 94],
-  ["2000-06-25", 71],
-  ["2000-06-26", 106],
-  ["2000-06-27", 84],
-  ["2000-06-28", 93],
-  ["2000-06-29", 85],
-  ["2000-06-30", 73],
-  ["2000-07-01", 83],
-  ["2000-07-02", 125],
-  ["2000-07-03", 107],
-  ["2000-07-04", 82],
-  ["2000-07-05", 44],
-  ["2000-07-06", 72],
-  ["2000-07-07", 106],
-  ["2000-07-08", 107],
-  ["2000-07-09", 66],
-  ["2000-07-10", 91],
-  ["2000-07-11", 92],
-  ["2000-07-12", 113],
-  ["2000-07-13", 107],
-  ["2000-07-14", 131],
-  ["2000-07-15", 111],
-  ["2000-07-16", 64],
-  ["2000-07-17", 69],
-  ["2000-07-18", 88],
-  ["2000-07-19", 77],
-  ["2000-07-20", 83],
-  ["2000-07-21", 111],
-  ["2000-07-22", 57],
-  ["2000-07-23", 55],
-  ["2000-07-24", 60]
-]
-
 const Pricing = () => {
   const { personalCheckout, isPersonalCheckoutLoading } = usePersonalCheckout()
   const { professionalCheckout, isProfessionalLoading } =
@@ -88,45 +35,16 @@ const Pricing = () => {
   const { insuredPolicyList, isInsuredListError, isInsuredListLoading } =
     useInsuredPolicies(address)
 
-  // const processPolicyData = (payerPolicyList) => {
-  //   const termMapping = {
-  //     0: "7 days",
-  //     1: "15 days",
-  //     2: "30 days"
-  //   }
+  const memoizedPayerPolicies = React.useMemo(
+    () => payerPolicyList,
+    [payerPolicyList]
+  )
+  const memoizedInsuredPolicies = React.useMemo(
+    () => insuredPolicyList,
+    [insuredPolicyList]
+  )
 
-  //   const typeMapping = {
-  //     0: "normal",
-  //     1: "advance"
-  //   }
-
-  //   return payerPolicyList.map((policy) => {
-  //     const endTime = new Date(Number(policy.endTime) * 1000)
-  //     const startTime = new Date(Number(policy.startTime) * 1000)
-
-  //     return {
-  //       ...policy,
-  //       policyTerm: termMapping[policy.Term],
-  //       policyType: typeMapping[policy.Type],
-  //       endTime: endTime.toLocaleDateString(),
-  //       startTime: startTime.toLocaleDateString(),
-  //       targetGasPrice: Number(policy.targetGasPrice),
-  //       volatility: Number(policy.volatility),
-  //       isExpired: Date.now() > endTime
-  //     }
-  //   })
-  // }
-
-  // const processedPayerPolicyList = processPolicyData(payerPolicyList || [])
-
-  // const processedInsuredPolicyList = processPolicyData(insuredPolicyList || [])
-
-  const payoutDates = [
-    "2023-01-15",
-    "2023-03-20",
-    "2023-05-10"
-    // ...其他赔付日期
-  ]
+  const payoutDates = ["2023-01-15", "2023-03-20", "2023-05-10"]
 
   return (
     <div className="overflow-hidden h-full mb-20 sm:py-32 lg:pb-32 xl:pb-36">
@@ -186,19 +104,6 @@ const Pricing = () => {
                       🔸 Protect You or Any ETH Address
                     </div>
                   </li>
-                  {/* <li className="flex flex-row gap-2">
-                    <div>Max Payout:</div>
-                    <div className="text-emerald-500">0.2 ETH</div>
-                  </li>
-                  <li className="flex flex-row gap-2">
-                    <div>Min Claim Fluctuation:</div>
-                    <div className="text-emerald-500">5%</div>
-                  </li>
-                  <li>Flexible Coverage</li>
-                  <li>Affordable Rates</li>
-                  <li>Immediate Protection</li>
-                  <li>Automated Claims Process</li>
-                  <li>Speedy Transactions</li> */}
                 </ul>
               </CardContent>
               <CardFooter className="flex justify-center items-center mt-20">
@@ -349,14 +254,14 @@ const Pricing = () => {
           {payerPolicyList ? (
             <>
               <div className="text-2xl mt-40">Insurance Policies as Payer</div>
-              <PolicyList policies={payerPolicyList} />
+              <PolicyList policies={memoizedPayerPolicies} />
             </>
           ) : null}
           {/* insured policy list */}
           {insuredPolicyList ? (
             <>
               <div className="text-2xl">Insurance Policies as Insured</div>
-              <PolicyList policies={insuredPolicyList} />
+              <PolicyList policies={memoizedInsuredPolicies} />
             </>
           ) : null}
         </div>

f308e994bf2293cd99f573188125762d7048a118
Ethan Ye <chiguayeshao@gmail.com>
2024-05-16 22:35:00 +0800

    update gasinsure list

--- components/gasInsure/GasInsureList.jsx
+++ components/gasInsure/GasInsureList.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react"
+import React, { useState, useEffect } from "react"
 import { Card } from "@/components/ui/card"
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs"
 import {
@@ -26,195 +26,214 @@ import {
   TooltipTrigger
 } from "@/components/ui/tooltip"
 import { SketchLogoIcon } from "@radix-ui/react-icons"
+import { ethers } from "ethers"
+import { useAccount } from "wagmi"
+import { Loader2 } from "lucide-react"
 
-// Simulated data for current policies
-const currentData = [
+const contractAddress = process.env.NEXT_PUBLIC_CONTRACT_ADDRESS
+const contractABI = [
   {
-    block: "003-004",
-    number: 100,
-    amount: "$10,000",
-    claimStatus: "yes",
-    points: "+15",
-    highRisk: "yes"
+    inputs: [
+      {
+        internalType: "address",
+        name: "_holder",
+        type: "address"
+      }
+    ],
+    name: "getPoliciesDetailsOfHolder",
+    outputs: [
+      {
+        components: [
+          {
+            internalType: "uint256",
+            name: "id",
+            type: "uint256"
+          },
+          {
+            internalType: "uint256",
+            name: "cycleId",
+            type: "uint256"
+          },
+          {
+            internalType: "uint256",
+            name: "units",
+            type: "uint256"
+          },
+          {
+            internalType: "address",
+            name: "holder",
+            type: "address"
+          },
+          {
+            internalType: "uint256",
+            name: "startBlock",
+            type: "uint256"
+          },
+          {
+            internalType: "bool",
+            name: "isClaimed",
+            type: "bool"
+          }
+        ],
+        internalType: "struct GasInsureV2.Policy[]",
+        name: "",
+        type: "tuple[]"
+      }
+    ],
+    stateMutability: "view",
+    type: "function"
   },
   {
-    block: "004-005",
-    number: 101,
-    amount: "$9,000",
-    claimStatus: "no",
-    points: "+10",
-    highRisk: "no"
-  },
-  {
-    block: "005-006",
-    number: 102,
-    amount: "$8,000",
-    claimStatus: "yes",
-    points: "+12",
-    highRisk: "yes"
-  },
-  {
-    block: "006-007",
-    number: 103,
-    amount: "$7,000",
-    claimStatus: "no",
-    points: "+8",
-    highRisk: "no"
-  },
-  {
-    block: "007-008",
-    number: 104,
-    amount: "$6,000",
-    claimStatus: "yes",
-    points: "+14",
-    highRisk: "yes"
-  },
-  {
-    block: "008-009",
-    number: 105,
-    amount: "$5,000",
-    claimStatus: "no",
-    points: "+7",
-    highRisk: "no"
-  },
-  {
-    block: "009-010",
-    number: 106,
-    amount: "$4,000",
-    claimStatus: "yes",
-    points: "+13",
-    highRisk: "yes"
-  },
-  {
-    block: "010-011",
-    number: 107,
-    amount: "$3,000",
-    claimStatus: "no",
-    points: "+6",
-    highRisk: "no"
-  },
-  {
-    block: "011-012",
-    number: 108,
-    amount: "$2,000",
-    claimStatus: "yes",
-    points: "+11",
-    highRisk: "yes"
-  },
-  {
-    block: "012-013",
-    number: 109,
-    amount: "$1,000",
-    claimStatus: "no",
-    points: "+5",
-    highRisk: "no"
-  }
-]
-
-// Simulated data for finished policies
-const finishedData = [
-  {
-    block: "001-002",
-    number: 200,
-    amount: "$20,000",
-    claimStatus: "no",
-    points: "+25",
-    highRisk: "no"
-  },
-  {
-    block: "002-003",
-    number: 201,
-    amount: "$19,000",
-    claimStatus: "yes",
-    points: "+22",
-    highRisk: "yes"
-  },
-  {
-    block: "003-004",
-    number: 202,
-    amount: "$18,000",
-    claimStatus: "no",
-    points: "+21",
-    highRisk: "no"
-  },
-  {
-    block: "004-005",
-    number: 203,
-    amount: "$17,000",
-    claimStatus: "yes",
-    points: "+18",
-    highRisk: "yes"
-  },
-  {
-    block: "005-006",
-    number: 204,
-    amount: "$16,000",
-    claimStatus: "no",
-    points: "+17",
-    highRisk: "no"
-  },
-  {
-    block: "006-007",
-    number: 205,
-    amount: "$15,000",
-    claimStatus: "yes",
-    points: "+14",
-    highRisk: "yes"
-  },
-  {
-    block: "007-008",
-    number: 206,
-    amount: "$14,000",
-    claimStatus: "no",
-    points: "+13",
-    highRisk: "no"
-  },
-  {
-    block: "008-009",
-    number: 207,
-    amount: "$13,000",
-    claimStatus: "yes",
-    points: "+12",
-    highRisk: "yes"
-  },
-  {
-    block: "009-010",
-    number: 208,
-    amount: "$12,000",
-    claimStatus: "no",
-    points: "+11",
-    highRisk: "no"
-  },
-  {
-    block: "010-011",
-    number: 209,
-    amount: "$11,000",
-    claimStatus: "yes",
-    points: "+10",
-    highRisk: "yes"
+    inputs: [
+      {
+        internalType: "uint256",
+        name: "",
+        type: "uint256"
+      }
+    ],
+    name: "cycles",
+    outputs: [
+      {
+        internalType: "uint256",
+        name: "id",
+        type: "uint256"
+      },
+      {
+        internalType: "uint256",
+        name: "startBlock",
+        type: "uint256"
+      },
+      {
+        internalType: "uint256",
+        name: "endBlock",
+        type: "uint256"
+      },
+      {
+        internalType: "uint256",
+        name: "coverBlock",
+        type: "uint256"
+      },
+      {
+        internalType: "uint256",
+        name: "premiumPerUnit",
+        type: "uint256"
+      },
+      {
+        internalType: "uint256",
+        name: "benefitPerUnit",
+        type: "uint256"
+      },
+      {
+        internalType: "uint256",
+        name: "units",
+        type: "uint256"
+      },
+      {
+        internalType: "bool",
+        name: "isActive",
+        type: "bool"
+      },
+      {
+        internalType: "bool",
+        name: "isClaimable",
+        type: "bool"
+      },
+      {
+        internalType: "bool",
+        name: "boost",
+        type: "bool"
+      }
+    ],
+    stateMutability: "view",
+    type: "function"
   }
 ]
 
 const GasInsureList = () => {
+  const { address, isConnected } = useAccount()
+  const [policies, setPolicies] = useState([])
+  const [loading, setLoading] = useState(true)
   const [currentPageCurrent, setCurrentPageCurrent] = useState(1)
   const [currentPageFinished, setCurrentPageFinished] = useState(1)
   const itemsPerPage = 3
 
+  useEffect(() => {
+    if (isConnected && address) {
+      fetchPolicies(address)
+    }
+
+    const interval = setInterval(() => {
+      if (isConnected && address) {
+        fetchPolicies(address)
+      }
+    }, 60000)
+
+    return () => clearInterval(interval)
+  }, [isConnected, address])
+
+  const fetchPolicies = async (address) => {
+    if (!window.ethereum) {
+      console.error("MetaMask is not installed")
+      return
+    }
+
+    try {
+      setLoading(true)
+      const provider = new ethers.providers.Web3Provider(window.ethereum)
+      const signer = provider.getSigner()
+      const contract = new ethers.Contract(contractAddress, contractABI, signer)
+
+      console.log(`Fetching policies for address: ${address}`)
+      const policiesDetails = await contract.getPoliciesDetailsOfHolder(address)
+      console.log(policiesDetails)
+
+      const detailedPolicies = await Promise.all(
+        policiesDetails.map(async (policy) => {
+          const cycle = await contract.cycles(policy.cycleId)
+          return {
+            id: policy.id.toString(),
+            cycleId: policy.cycleId.toString(),
+            units: policy.units.toString(),
+            holder: policy.holder,
+            startBlock: policy.startBlock.toString(),
+            isClaimed: policy.isClaimed,
+            endBlock: cycle.endBlock.toString(),
+            amount: ethers.utils.formatEther(
+              (policy.units * cycle.premiumPerUnit).toString()
+            ),
+            isActive: cycle.isActive,
+            isClaimable: cycle.isClaimable,
+            highRisk: cycle.boost
+          }
+        })
+      )
+
+      setPolicies(detailedPolicies)
+    } catch (error) {
+      console.error(`Error fetching policies: ${error}`)
+    } finally {
+      setLoading(false)
+    }
+  }
+
+  const currentPolicies = policies.filter((policy) => policy.isActive)
+  const finishedPolicies = policies.filter((policy) => !policy.isActive)
+
   // Calculate paginated data for current policies
   const offsetCurrent = (currentPageCurrent - 1) * itemsPerPage
-  const currentPageDataCurrent = currentData.slice(
+  const currentPageDataCurrent = currentPolicies.slice(
     offsetCurrent,
     offsetCurrent + itemsPerPage
   )
-  const pageCountCurrent = Math.ceil(currentData.length / itemsPerPage)
+  const pageCountCurrent = Math.ceil(currentPolicies.length / itemsPerPage)
 
   // Calculate paginated data for finished policies
   const offsetFinished = (currentPageFinished - 1) * itemsPerPage
-  const currentPageDataFinished = finishedData.slice(
+  const currentPageDataFinished = finishedPolicies.slice(
     offsetFinished,
     offsetFinished + itemsPerPage
   )
-  const pageCountFinished = Math.ceil(finishedData.length / itemsPerPage)
+  const pageCountFinished = Math.ceil(finishedPolicies.length / itemsPerPage)
 
   const renderPaginationItems = (currentPage, pageCount, handlePageChange) => {
     const items = []
@@ -239,7 +258,12 @@ const GasInsureList = () => {
 
   return (
     <>
-      <Card className="w-full h-full flex items-center justify-center border-2 border-[#159895]">
+      <Card className="w-full h-full flex items-center justify-center border-2 border-[#159895] relative">
+        {loading && (
+          <div className="absolute inset-0 bg-white bg-opacity-75 flex items-center justify-center">
+            <Loader2 className="animate-spin h-10 w-10 text-[#159895]" />
+          </div>
+        )}
         <div className="h-full w-full">
           <Tabs defaultValue="current" className="w-full p-2">
             <div className="flex flex-row justify-between">
@@ -274,27 +298,41 @@ const GasInsureList = () => {
                 <TableCaption>A list of your current policy.</TableCaption>
                 <TableHeader>
                   <TableRow>
-                    <TableHead className="w-[100px]">Block</TableHead>
-                    <TableHead>Number</TableHead>
-                    <TableHead>Amount</TableHead>
-                    <TableHead>Claim status</TableHead>
-                    <TableHead>Points</TableHead>
-                    <TableHead>High Risk</TableHead>
-                    <TableHead className="text-right"></TableHead>
+                    <TableHead className="w-[120px]">Start Block</TableHead>
+                    <TableHead className="w-[120px]">End Block</TableHead>
+                    <TableHead className="w-[120px]">Amount</TableHead>
+                    <TableHead className="w-[120px]">Claim status</TableHead>
+                    <TableHead className="w-[120px]">Points</TableHead>
+                    <TableHead className="w-[120px]">High Risk</TableHead>
+                    <TableHead className="w-[120px] text-right"></TableHead>
                   </TableRow>
                 </TableHeader>
                 <TableBody>
-                  {currentPageDataCurrent.map((item, index) => (
+                  {currentPageDataCurrent.map((policy, index) => (
                     <TableRow key={index}>
-                      <TableCell className="w-[100px]">{item.block}</TableCell>
-                      <TableCell>{item.number}</TableCell>
-                      <TableCell>{item.amount}</TableCell>
-                      <TableCell>{item.claimStatus}</TableCell>
-
-                      <TableCell>{item.points}</TableCell>
-                      <TableCell className="">{item.highRisk}</TableCell>
-                      <TableCell className="text-right">
-                        <Button className="bg-[#57C5B6] text-white transform hover:scale-105 hover:bg-[#159895]">
+                      <TableCell className="w-[120px]">
+                        {policy.startBlock}
+                      </TableCell>
+                      <TableCell className="w-[120px]">
+                        {policy.endBlock === "0" ? "pending" : policy.endBlock}
+                      </TableCell>
+                      <TableCell className="w-[120px]">
+                        {policy.amount} ETH
+                      </TableCell>
+                      <TableCell className="w-[120px]">
+                        {policy.isClaimed ? "Yes" : "No"}
+                      </TableCell>
+                      <TableCell className="w-[120px]">
+                        Points Placeholder
+                      </TableCell>
+                      <TableCell className="w-[120px]">
+                        {policy.highRisk ? "Yes" : "No"}
+                      </TableCell>
+                      <TableCell className="w-[120px] text-right">
+                        <Button
+                          className="bg-[#57C5B6] text-white transform hover:scale-105 hover:bg-[#159895]"
+                          disabled={!policy.isClaimable}
+                        >
                           claim
                         </Button>
                       </TableCell>
@@ -339,27 +377,41 @@ const GasInsureList = () => {
                 <TableCaption>A list of your finished policy.</TableCaption>
                 <TableHeader>
                   <TableRow>
-                    <TableHead className="w-[100px]">Block</TableHead>
-                    <TableHead>Number</TableHead>
-                    <TableHead>Amount</TableHead>
-                    <TableHead>Claim status</TableHead>
-                    <TableHead>Points</TableHead>
-                    <TableHead>High Risk</TableHead>
-                    <TableHead className="text-right"></TableHead>
+                    <TableHead className="w-[120px]">Start Block</TableHead>
+                    <TableHead className="w-[120px]">End Block</TableHead>
+                    <TableHead className="w-[120px]">Amount</TableHead>
+                    <TableHead className="w-[120px]">Claim status</TableHead>
+                    <TableHead className="w-[120px]">Points</TableHead>
+                    <TableHead className="w-[120px]">High Risk</TableHead>
+                    <TableHead className="w-[120px] text-right"></TableHead>
                   </TableRow>
                 </TableHeader>
                 <TableBody>
-                  {currentPageDataFinished.map((item, index) => (
+                  {currentPageDataFinished.map((policy, index) => (
                     <TableRow key={index}>
-                      <TableCell className="w-[100px]">{item.block}</TableCell>
-                      <TableCell>{item.number}</TableCell>
-                      <TableCell>{item.amount}</TableCell>
-                      <TableCell>{item.claimStatus}</TableCell>
-
-                      <TableCell>{item.points}</TableCell>
-                      <TableCell className="">{item.highRisk}</TableCell>
-                      <TableCell className="text-right">
-                        <Button className="bg-[#57C5B6] text-white transform hover:scale-105 hover:bg-[#159895]">
+                      <TableCell className="w-[120px]">
+                        {policy.startBlock}
+                      </TableCell>
+                      <TableCell className="w-[120px]">
+                        {policy.endBlock === "0" ? "pending" : policy.endBlock}
+                      </TableCell>
+                      <TableCell className="w-[120px]">
+                        {policy.amount} ETH
+                      </TableCell>
+                      <TableCell className="w-[120px]">
+                        {policy.isClaimed ? "Yes" : "No"}
+                      </TableCell>
+                      <TableCell className="w-[120px]">
+                        Points Placeholder
+                      </TableCell>
+                      <TableCell className="w-[120px]">
+                        {policy.highRisk ? "Yes" : "No"}
+                      </TableCell>
+                      <TableCell className="w-[120px] text-right">
+                        <Button
+                          className="bg-[#57C5B6] text-white transform hover:scale-105 hover:bg-[#159895]"
+                          disabled={!policy.isClaimable}
+                        >
                           claim
                         </Button>
                       </TableCell>

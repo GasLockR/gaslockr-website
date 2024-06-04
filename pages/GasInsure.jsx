f7a35b56c97533266291c78cea6566a4b995f47e
Ethan Ye <chiguayeshao@gmail.com>
2024-06-04 12:29:20 +0800

    update contract

--- pages/GasInsure.jsx
+++ pages/GasInsure.jsx
@@ -16,23 +16,23 @@ const GasInsure = () => {
     }
   }, [chain, switchNetwork])
 
-  return <MaintenancePage />
+  // return <MaintenancePage />
 
-  // return (
-  //   <Container>
-  //     <div className="flex flex-col lg:flex-row gap-4 mt-8">
-  //       <div className="flex-1 w-full lg:w-3/4">
-  //         <GasChart />
-  //       </div>
-  //       <div className="w-full lg:w-1/4">
-  //         <GasInsureOrder />
-  //       </div>
-  //     </div>
-  //     <div className="h-2/5 py-4">
-  //       <GasInsureList />
-  //     </div>
-  //   </Container>
-  // )
+  return (
+    <Container>
+      <div className="flex flex-col lg:flex-row gap-4 mt-8">
+        <div className="flex-1 w-full lg:w-3/4">
+          <GasChart />
+        </div>
+        <div className="w-full lg:w-1/4">
+          <GasInsureOrder />
+        </div>
+      </div>
+      <div className="h-2/5 py-4">
+        <GasInsureList />
+      </div>
+    </Container>
+  )
 }
 
 export default GasInsure
